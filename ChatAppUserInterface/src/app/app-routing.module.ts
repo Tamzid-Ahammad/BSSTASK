274a927ffd92d3ef8e58ba66fc3ec3a277767644
Gazitamzidahammad <Gazitamzidahammad@gmail.com>
2024-09-23 18:22:08 +0600

    Set the routing module

--- ChatAppUserInterface/src/app/app-routing.module.ts
+++ ChatAppUserInterface/src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { RegistrationComponent } from './user/registration/registration.component';
+import { LoginComponent } from './user/login/login.component';
+import { HomeComponent } from './home/home.component';
+import { AccessdeniedComponent } from './accessdenied/accessdenied.component';
+import { authGuard } from './guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/user/login', pathMatch: 'full' },
+  {
+    path: 'user', component: UserComponent,
+    children: [
+      { path: 'registration', component: RegistrationComponent },
+      { path: 'login', component: LoginComponent }
+    ]
+  },
+  { path: 'home', component: HomeComponent, canActivate: [authGuard] },
+  { path: 'accessdenied', component: AccessdeniedComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
