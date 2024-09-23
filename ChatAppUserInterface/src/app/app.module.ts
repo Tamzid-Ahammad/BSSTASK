33262eff987212b98f3520536a437e17f8d1774e
Gazitamzidahammad <Gazitamzidahammad@gmail.com>
2024-09-23 16:26:57 +0600

    Made Home Component For Chating Place

--- ChatAppUserInterface/src/app/app.module.ts
+++ ChatAppUserInterface/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { UserComponent } from './user/user.component';
 import { LoginComponent } from './user/login/login.component';
 import { RegistrationComponent } from './user/registration/registration.component';
 import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { RouterModule } from '@angular/router';
     AccessdeniedComponent,
     UserComponent,
     LoginComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
