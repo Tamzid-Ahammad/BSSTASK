d8dd1bf81f84c5b2f02f43ac2e8328d9ec41fdf8
Gazitamzidahammad <Gazitamzidahammad@gmail.com>
2024-09-23 11:44:35 +0600

    Create Environment

--- ChatAppUserInterface/src/app/guards/auth.guard.ts
+++ ChatAppUserInterface/src/app/guards/auth.guard.ts
@@ -1,12 +1,9 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
 
-
 @Injectable({
   providedIn: 'root'
 })
-
-
 
 export class authGuard  implements CanActivate {
   constructor(private router: Router) {
---
+++ ChatAppUserInterface/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'https://localhost:7104/api',
+  chatHubUrl: "https://localhost:7104/chathub"
+};
